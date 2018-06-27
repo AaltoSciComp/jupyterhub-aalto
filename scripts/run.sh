42117a60f7c2b871d461282dd1d9f632ead22692
root <root@jupyter.cs.aalto.fi>
2018-06-27 18:53:07 +0300

    improve run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,14 @@
 cat /etc/adpw.txt | join_ad.sh
 service sssd start
 tries=0
-while [ ! id murhum1 && $tries -lt 10 ]; do
-  tries = $(($tries+1));
-  echo "Try $tries, sleeping..."
+while true;
+do
+  id murhum1
+  if [ $? -eq 0 ]; then
+    break;
+  fi
+  tries=$(($tries+1));
+  echo "Trying to connect to aalto ldap: $tries tries so far..."
   sleep 10;
 done
 jupyterhub
