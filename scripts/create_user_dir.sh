abec9cd57b3513073fefba799162e12c29ce5da0
Richard Darst <rkd@zgib.net>
2018-07-06 16:40:35 +0300

    scripts/create_user_dir: lookup uid locally for security, alaways mkdir

--- scripts/create_user_dir.sh
+++ scripts/create_user_dir.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+set -x
 username=$1
-uid=$2
+uid=$(id -u $username)
+
+if [ -z "$uid" ] ; then
+  uid="$2"
+fi
 
 dir_name=/mnt/jupyter/user/$username
 
-if [ ! -d $dir_name ]; then
-  mkdir $dir_name 2>/dev/null
+#if [ ! -d $dir_name ]; then
+  mkdir -p $dir_name 2>/dev/null
   chown $uid $dir_name
   chmod 700 $dir_name
-fi
+#fi
