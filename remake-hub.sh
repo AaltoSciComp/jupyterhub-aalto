166b860ef9ee38be704e4561c6167f240e007422
Richard Darst <rkd@zgib.net>
2018-07-27 15:54:19 +0300

    ./remake-hub: delete proxy pid file
    - Instead we should find a way to make this non-persistent.

--- remake-hub.sh
+++ remake-hub.sh
@@ -1,4 +1,6 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 $SCRIPTPATH/delete-hub.sh
+# sometimes this proxy pid needs deletion... eventually find a better solution.
+rm /mnt/jupyter/admin/hubdata/jupyterhub-proxy.pid
 $SCRIPTPATH/create-hub.sh
