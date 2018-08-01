7e4c8d8e5a54816126888635c63a5725be064ef0
Richard Darst <rkd@zgib.net>
2018-08-01 11:49:51 +0300

    delete-hub.sh: Add commented command to delete stuck images

--- delete-hub.sh
+++ delete-hub.sh
@@ -2,3 +2,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 kubectl delete configmap jupyterhub-config
 kubectl delete -f $SCRIPTPATH/jupyterhub.yaml
+
+# Delete non-stopping pods:
+# kubectl delete pod --force --grace-period=0 `kubectl get pods | grep Termin | grep jupyterhub | cut -d\  -f1`
+
