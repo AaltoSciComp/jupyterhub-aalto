0d9751034e82517a1eaf55f8fac002e6f64318a2
Richard Darst <rkd@zgib.net>
2018-08-18 00:08:09 +0300

    remake-hub.sh: don't complain if proxy pid file missing (not used anymore)

--- remake-hub.sh
+++ remake-hub.sh
@@ -2,5 +2,5 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 $SCRIPTPATH/delete-hub.sh
 # sometimes this proxy pid needs deletion... eventually find a better solution.
-rm /mnt/jupyter/admin/hubdata/jupyterhub-proxy.pid
+rm -f /mnt/jupyter/admin/hubdata/jupyterhub-proxy.pid
 $SCRIPTPATH/create-hub.sh
