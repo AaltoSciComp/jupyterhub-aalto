b6c36dcc034bc17b717dc7557e833e313a536651
Markus Murhu <markus.murhu@aalto.fi>
2018-09-04 16:50:03 +0300

    Update hub status script config maps when restart

--- create-hub.sh
+++ create-hub.sh
@@ -1,4 +1,5 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 kubectl create configmap jupyterhub-config --from-file=$SCRIPTPATH/jupyterhub_config.py
+kubectl create configmap hub-status-service --from-file=$SCRIPTPATH/scripts/hub_status_service.py
 kubectl create -f $SCRIPTPATH/jupyterhub.yaml
--- delete-hub.sh
+++ delete-hub.sh
@@ -2,6 +2,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 kubectl delete configmap jupyterhub-config
 kubectl delete -f $SCRIPTPATH/jupyterhub.yaml
+kubectl delete configmap hub-status-service
 
 # Delete non-stopping pods:
 # kubectl delete pod --force --grace-period=0 `kubectl get pods | grep Termin | grep jupyterhub | cut -d\  -f1`
