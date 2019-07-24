5b6c779a9fb624183589ce349467e86169e0a7ff
Richard Darst <rkd@zgib.net>
2019-07-25 01:26:15 +0300

    Add syntax error to jupyterhub_config.py before restarting

--- bin/remake-hub.sh
+++ bin/remake-hub.sh
@@ -1,5 +1,11 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+# Syntax check the hub config file first.
+if ! python3 -m py_compile jupyterhub_config.py ; then
+    echo "jupyterhub_config.py has invalid syntax, aborting the hub restart."
+    exit
+fi
+
 $SCRIPTPATH/delete-hub.sh
 # sometimes this proxy pid needs deletion... eventually find a better solution.
 rm -f /mnt/jupyter/admin/hubdata/jupyterhub-proxy.pid
