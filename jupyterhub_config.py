5db7cb6b73ad3ec9f0dc39166dd76a73abc820bd
Richard Darst <rkd@zgib.net>
2019-08-05 23:22:47 +0300

    Bump default image to 1.0.2

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -14,10 +14,10 @@ import yaml
 
 # c.JupyterHub.log_level = 'DEBUG'
 
-IMAGE_DEFAULT = 'aaltoscienceit/notebook-server:1.0.0'
+IMAGE_DEFAULT = 'aaltoscienceit/notebook-server:1.0.2'
 IMAGE_DEFAULT_R = 'aaltoscienceit/notebook-server-r-ubuntu:1.0.0'
 IMAGE_DEFAULT_JULIA = 'aaltoscienceit/notebook-server-julia:1.0.0'
-IMAGE_TESTING = 'aaltoscienceit/notebook-server:1.0.2'
+#IMAGE_TESTING = 'aaltoscienceit/notebook-server:1.0.2'
 IMAGES_OLD = [
     'aaltoscienceit/notebook-server:1.0.0',
 ]
