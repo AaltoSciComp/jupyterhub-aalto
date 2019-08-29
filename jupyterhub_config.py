9dfb0e7a2b911c6ea10b22bdde5c24c4f9be1b31
Sami Laine <sami.v.laine@aalto.fi>
2019-08-29 14:21:47 +0300

    Add laines5 as admin

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -134,7 +134,7 @@ class NormalizingPAMAuthenticator(PAMAuthenticator):
         return super().normalize_username(pwd.getpwuid(uid).pw_name)
 c.JupyterHub.authenticator_class = NormalizingPAMAuthenticator
 #c.Authenticator.delete_invalid_users = True  # delete users once no longer in Aalto AD
-c.Authenticator.admin_users = {'darstr1', }
+c.Authenticator.admin_users = {'darstr1', 'laines5'}
 USER_RE = re.compile('^[a-z0-9.]+$')
 
 
