781a15a223a293de7597fccbca7968dde267a878
Richard Darst <rkd@zgib.net>
2019-09-18 22:18:31 +0300

    Update default images (1.8.1)

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -14,10 +14,10 @@ import yaml
 
 # c.JupyterHub.log_level = 'DEBUG'
 
-IMAGE_DEFAULT = 'aaltoscienceit/notebook-server:1.8.0'         # for generic images
+IMAGE_DEFAULT = 'aaltoscienceit/notebook-server:1.8.1'         # for generic images
 IMAGE_COURSE_DEFAULT = 'aaltoscienceit/notebook-server:1.7.0'  # for courses
-IMAGE_DEFAULT_R = 'aaltoscienceit/notebook-server-r-ubuntu:1.8.0'
-IMAGE_DEFAULT_JULIA = 'aaltoscienceit/notebook-server-julia:1.8.0'
+IMAGE_DEFAULT_R = 'aaltoscienceit/notebook-server-r-ubuntu:1.8.1'
+IMAGE_DEFAULT_JULIA = 'aaltoscienceit/notebook-server-julia:1.8.1'
 IMAGE_DEFAULT_CUDA = 'aaltoscienceit/notebook-server-cuda:1.7.0'
 #IMAGE_TESTING = 'aaltoscienceit/notebook-server:1.0.6'
 IMAGES_OLD = [
