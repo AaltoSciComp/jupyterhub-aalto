aa73dbb6dc73ad116b90045ed0cd6566bc007992
Richard Darst <rkd@zgib.net>
2020-08-04 11:30:01 +0300

    scripts/cull_idle_servers: add max() to prevent minimum cull times
    - TODO: what is the best way to handle this?

--- scripts/cull_idle_servers.py
+++ scripts/cull_idle_servers.py
@@ -188,9 +188,9 @@ def cull_idle(
         if 'child_conf' in state:
             state = state['child_conf']
         if 'cull_max_age' in state:
-            max_age = state['cull_max_age']
+            max_age = max(max_age, state['cull_max_age'])
         if 'cull_inactive_time' in state:
-            inactive_limit = state['cull_inactive_time']
+            inactive_limit = max(inactive_limit, state['cull_inactive_time'])
         app_log.info(f"CULL IDLE: {user['name']}/{server_name}: {max_age} inactive={inactive} inactive_limit={inactive_limit} age={age} last_activity={server['last_activity']}")
 
         should_cull = (
