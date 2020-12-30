711da355b10ed8f2d2e099319dda345b7ddc231c
Richard Darst <rkd@zgib.net>
2020-12-30 21:59:11 +0200

    jupyterhub_config: Update default images to 4.0.0

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -22,10 +22,10 @@ import string
 
 # c.JupyterHub.log_level = 'DEBUG'
 
-IMAGE_DEFAULT = 'aaltoscienceit/notebook-server:3.0.5'         # for generic images
-IMAGE_COURSE_DEFAULT = 'aaltoscienceit/notebook-server:3.0.5'  # for courses
-IMAGE_DEFAULT_R = 'aaltoscienceit/notebook-server-r-ubuntu:3.0.7'
-IMAGE_DEFAULT_JULIA = 'aaltoscienceit/notebook-server-julia:3.0.0'
+IMAGE_DEFAULT = 'aaltoscienceit/notebook-server:4.0.0'         # for generic images
+IMAGE_COURSE_DEFAULT = 'aaltoscienceit/notebook-server:4.0.0'  # for courses
+IMAGE_DEFAULT_R = 'aaltoscienceit/notebook-server-r-ubuntu:4.0.0'
+IMAGE_DEFAULT_JULIA = 'aaltoscienceit/notebook-server-julia:4.0.0'
 IMAGE_DEFAULT_CUDA = 'aaltoscienceit/notebook-server-cuda:1.8.8'
 #IMAGE_TESTING = 'aaltoscienceit/notebook-server:1.0.6'
 IMAGES_OLD = [
