c3686d5c9a6784e61535cc9425e6d4916c4efe06
Richard Darst <rkd@zgib.net>
2021-02-04 00:11:46 +0200

    jupyterhub_config: 4.1.5

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -22,9 +22,9 @@ import string
 
 # c.JupyterHub.log_level = 'DEBUG'
 
-IMAGE_DEFAULT = 'aaltoscienceit/notebook-server:4.1.3'         # for generic images
-IMAGE_COURSE_DEFAULT = 'aaltoscienceit/notebook-server:4.1.3'  # for courses
-IMAGE_DEFAULT_R = 'aaltoscienceit/notebook-server-r-ubuntu:4.1.0'
+IMAGE_DEFAULT = 'aaltoscienceit/notebook-server:4.1.5'         # for generic images
+IMAGE_COURSE_DEFAULT = 'aaltoscienceit/notebook-server:4.1.5'  # for courses
+IMAGE_DEFAULT_R = 'aaltoscienceit/notebook-server-r-ubuntu:4.1.5'
 IMAGE_DEFAULT_JULIA = 'aaltoscienceit/notebook-server-julia:4.1.0'
 IMAGE_DEFAULT_CUDA = 'aaltoscienceit/notebook-server-cuda:1.8.8'
 #IMAGE_TESTING = 'aaltoscienceit/notebook-server:1.0.6'
