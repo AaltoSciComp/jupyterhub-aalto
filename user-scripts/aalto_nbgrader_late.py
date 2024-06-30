c4ea341afc6b6cc54e64eba7b65eda3709937234
Alireza Tajmirriahi <alireza.tajmirriahi@aalto.fi>
2024-06-30 21:29:35 +0300

    Make penalty unit configurable in custom plugins

--- user-scripts/aalto_nbgrader_late.py
+++ user-scripts/aalto_nbgrader_late.py
@@ -2,12 +2,12 @@ from __future__ import division
 from nbgrader.plugins import BasePlugin
 
 from textwrap import dedent
-from traitlets import List
+from traitlets import List, Float
 
 from math import ceil
 
 
-class AaltoNbGraderPlugin(BasePlugin):
+class AaltoBasePlugin(BasePlugin):
     student_exemptions = List(
         default_value=[],
         help=dedent(
@@ -18,33 +18,51 @@ class AaltoNbGraderPlugin(BasePlugin):
         )
     ).tag(config=True)
 
+    penalty_unit = Float(
+        default_value=0.0,
+        help=dedent(
+            """
+            The penalty unit for each plugin. For instance, the value 0.2 for `SubRatio`
+            means that submission will receive 20% penalty; whereas the value 1 for `SubMarks`
+            means 1 mark penalty per each hour late
+            """
+        )
+    ).tag(config=True)
 
-class SubMarks(BasePlugin):
     def late_submission_penalty(self, student_id, score, total_seconds_late):
-        """Penalty of 1 mark per hour late"""
-        penalty_unit = 1
-        hours_late = round(total_seconds_late / 3600, 0)
-        return hours_late * penalty_unit
+        raise NotImplementedError
 
 
-class SubStep(BasePlugin):
+class SubMarks(AaltoBasePlugin):
     def late_submission_penalty(self, student_id, score, total_seconds_late):
-        """Penalty of 10 marks per every three hours late"""
-
-        penalty_unit = 10
-        hours_late = total_seconds_late / 3600
-        three_hours = ceil(hours_late / 3)
-        return three_hours * penalty_unit
-
-
-class SubRatio(AaltoNbGraderPlugin):
-    def late_submission_penalty(self, student_id, score, total_seconds_late):
-        """Penalty of 20% score per day late"""
+        """Penalty of fixed marks per hour late"""
 
         if student_id in self.student_exemptions:
             return 0
 
-        penalty_unit = 0.2 * score
+        hours_late = round(total_seconds_late / 3600, 0)
+        return hours_late * self.penalty_unit
+
+
+class SubStep(AaltoBasePlugin):
+    def late_submission_penalty(self, student_id, score, total_seconds_late):
+        """Penalty of fixed marks per every three hours late"""
+
+        if student_id in self.student_exemptions:
+            return 0
+
+        hours_late = total_seconds_late / 3600
+        three_hours = ceil(hours_late / 3)
+        return three_hours * self.penalty_unit
+
+
+class SubRatio(AaltoBasePlugin):
+    def late_submission_penalty(self, student_id, score, total_seconds_late):
+        """Penalty of fixed ratio per day late"""
+
+        if student_id in self.student_exemptions:
+            return 0
+
         hours_late = total_seconds_late / 3600
         day_late = ceil(hours_late / 24)
-        return day_late * penalty_unit
+        return day_late * self.penalty_unit * score
