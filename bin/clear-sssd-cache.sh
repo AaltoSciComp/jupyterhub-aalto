3138860ef13efffc5fe94d1da9005bc6f75f931a
Sami Laine <sami.v.laine@aalto.fi>
2025-04-11 16:17:05 +0300

    Add script to clear sssd cache

---
+++ bin/clear-sssd-cache.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+NAMESPACE=${1:-jupyter}
+# shellcheck source-path=bin
+source "$SCRIPTPATH/_check_namespace.sh"
+
+kubectl exec -n "$NAMESPACE" deploy/jupyterhub -- bash -c "service sssd stop ; rm /var/lib/sss/db/* ; service sssd start ; getent group jupyter-testcourse"
+echo "SSSD cache cleared"
