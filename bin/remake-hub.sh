18c8e279cc42ba96e1503bf66dd6854e5439f92f
Sami Laine <sami.v.laine@aalto.fi>
2025-04-11 16:16:48 +0300

    Run shellcheck on scripts

--- bin/remake-hub.sh
+++ bin/remake-hub.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-set -uo pipefail
+set -euo pipefail
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 NAMESPACE=${1:-jupyter}
+# shellcheck source-path=bin
 source "$SCRIPTPATH/_check_namespace.sh"
 
 JMGR_HOSTNAME=root@jupyter-manager-2.cs.aalto.fi
 
 # Syntax check the hub config file first.
-if ! python3 -m py_compile $SCRIPTPATH/../jupyterhub_config.py ; then
+if ! python3 -m py_compile "$SCRIPTPATH/../jupyterhub_config.py" ; then
     echo "jupyterhub_config.py has invalid syntax, aborting the hub restart."
     exit
 fi
 
 echo "Stopping hub"
-$SCRIPTPATH/delete-hub.sh $NAMESPACE
+"$SCRIPTPATH/delete-hub.sh" "$NAMESPACE"
 # sometimes this proxy pid needs deletion... eventually find a better solution.
 if [ "$NAMESPACE" = "jupyter" ]; then
     JUPYTER_PATH=/mnt/jupyter
@@ -29,4 +30,4 @@ timeout 2 ssh $JMGR_HOSTNAME "mkdir -p \
     && chmod o-rwx $JUPYTER_PATH/admin"
 
 echo "Starting hub"
-$SCRIPTPATH/create-hub.sh $NAMESPACE
+"$SCRIPTPATH/create-hub.sh" "$NAMESPACE"
